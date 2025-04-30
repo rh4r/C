aeb62dcd3609df4714a14f10df359e9ab0e59a3e
rher <notequalnil@gmail.com>
2025-04-30 14:17:48 +0100

    baseplate

---
+++ C++/WizardBattle.cpp
@@ -0,0 +1,9 @@
+#include <cmath> // Round function
+#include <string> // String functionability
+#include <iostream> // "Cout" and "Cin" functions
+#include <stdlib.h> // Sleep function (takes input in miliseconds, so 2000 = 2 seconds)
+
+int main(){
+  std::cout << "hello";
+  return 0;
+}
