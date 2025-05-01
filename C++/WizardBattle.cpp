80ff4754c80a2759db9489f927bd2a22c9762593
rher <notequalnil@gmail.com>
2025-05-01 15:41:33 +0100

    Update WizardBattle.cpp

--- C++/WizardBattle.cpp
+++ C++/WizardBattle.cpp
@@ -1,67 +1,35 @@
-#include <cmath> // Round function
-#include <string> // String functionability
-#include <iostream> // "Cout" and "Cin" functions
-#include <stdlib.h> // Sleep function (takes input in miliseconds, so 2000 = 2 seconds)
-#include <vector> // Array functionality
-
-const std::vector<std::string> StoredWizards;
-const std::string BattleWinner;
-const std::string Banner = R"(
-██╗    ██╗██╗███████╗ █████╗ ██████╗ ██████╗      ██╗ ██████╗  ██╗
-██║    ██║██║╚══███╔╝██╔══██╗██╔══██╗██╔══██╗    ███║██╔═████╗███║
-██║ █╗ ██║██║  ███╔╝ ███████║██████╔╝██║  ██║    ╚██║██║██╔██║╚██║
-██║███╗██║██║ ███╔╝  ██╔══██║██╔══██╗██║  ██║     ██║████╔╝██║ ██║
-╚███╔███╔╝██║███████╗██║  ██║██║  ██║██████╔╝     ██║╚██████╔╝ ██║
-╚══╝╚══╝ ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝      ╚═╝ ╚═════╝  ╚═╝
-
-)";
-
-class Wizard {
-  public:
-  int Health;
-  int Wisdom;
-  std::string Name;
-  std::string Element;
-};
-
-void PrintArray(array){
-  int Count = 1
-
-  for (int index; index <= sizeof(array); index++){
-    std::cout << Count << " " << index;
-    Count += 1;
+  string placeholder_string;
+  // Fire advantages or disadvantages
+  map<string, float> fire_values = {
+    {"water": 0.5},
+    {"air": 1.2},
+    {"earth": 1.5},
+    {"fire": 1},
   }
-}
-
-void CreateWizard(wizard_name, element){
-  if (wizard_name != NULL; element != NULL){
-    Wizard FirstWizardObject;
-    StoredWizards.push_back(FirstWizardObject);
+  // Water advantages or disadvantages
+    map<string, float> water_values = {
+    {"water": 1},
+    {"air": 1},
+    {"earth": 0.8},
+    {"fire": 1.5},
   }
-}
-
-std::string ReturnLowerStr(inputted_string){
-  return tolower(inputted_string);
-}
-
-std::string ReturnLoweredStr(element){
-  std::string placeholder_string;
-  
-  for (int index; index <= length(element);index++){
-    char current_character = element[index];
-
-    if (isupper(current_char)){
-      placeholder_string += tolower(current_character);
-    }
+  // Air advantages or disadvantages
+    map<string, float> air_values = {
+    {"water": 1},
+    {"air": 1},
+    {"earth": 1},
+    {"fire": 0.8},
   }
-  return placeholder_string;
-}
-
-int main(){
-  std::cout << Banner << std::endl;
-
-  while (sizeof(StoredWizards) < 2){
-    InitializeInputs();
+  // Earth advantages or disadvantages
+    map<string, float> earth_values = {
+    {"water": 1.2},
+    {"air": 0.5},
+    {"earth": 1},
+    {"fire": 0.5},
   }
-  return 0;
-}
+
+  int index = 0;
+  for (const auto& [key, value] : fire_values){
+    cout << "Index: " << index << ", Key: " << key << ", Value: " << value;
+    ++index;
+  }
